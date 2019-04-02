d8fd91b51b90479b485cb0f093df491984cd66fa
ZLZLab <zhengzongli@gmail.com>
2019-04-03 01:18:53 +0800

    v0.1

---
+++ README.md
@@ -0,0 +1,32 @@
+# rideo - An R script for making ideogram
+## by: zhengzongli@gmail.com
+
+## 1. git clone:
+
+
+## 2. Prepare an input file that contains a list of targets. This file must include five fields.
+ E.g.: GUIDE-Seq_target.list.txt
+
+	#Chromosome     Start   End     Targetsite      Mismatches
+	chr2    73160981        73161004        EMX1    0
+	chr5    45359060        45359083        EMX1    2
+	chr2    219845055       219845078       EMX1    3
+	chr15   65637530        65637553        VEGFA_site1     2
+	chr6    43737290        43737313        VEGFA_site1     0
+	chr1    99347644        99347667        VEGFA_site1     3
+
+ The field Targetsite is the sgRNA name.
+ The field Mismatches being '0' means on-target, and >= 1 means off-target
+
+## 3. Run the following command
+
+	Rscript  rideo.R  /your/full/path/to/git/rideo  GUIDE-Seq_target.list.txt
+
+	#==== In the 'output' folder, you will get a pdf file for each of the Targetsite, such as:
+	#	output/GUIDE-Seq_target.list.EMX1.pdf 
+	#	output/GUIDE-Seq_target.list.VEGFA_site1.pdf 
+
+## 4. Make your own target list file (contains the 5 fields), and run:
+
+	Rscript  rideo.R  /your/full/path/to/git/rideo  MyTargetList.txt
+
---
+++ rideo.R
@@ -0,0 +1,63 @@
+
+# Run command:
+#	 Rscript  rideo.R  /your/full/path/to/git/rideo  GUIDE-Seq_target.list.txt
+
+rideoPath = commandArgs(TRUE)[1]
+targetFile = commandArgs(TRUE)[2]
+
+fileName = sub('.txt', '', targetFile)
+
+cyto = read.table(paste(rideoPath, '/cytoband.txt', sep=''), header=T, stringsAsFactors=F)
+	#head(cyto)
+targetFileData = read.table(targetFile, sep='\t', stringsAsFactors=F, header=F)
+	#head(targetFileData)
+	names(targetFileData) = c('Chromosome', 'Start', 'End', 'Targetsite',  'Mismatches')
+
+	## add 'chr' if not already
+	if (substr(targetFileData$Chromosome[1], 1, 3) != 'chr') {
+		targetFileData$Chromosome = paste0('chr', targetFileData$Chromosome)
+	}
+chrs = unique(cyto$Chromosome)
+sites = unique(targetFileData$Targetsite)
+
+dir.create('output', showWarnings = FALSE)
+##==== Output one pdf file for each site
+for (site in sites){
+	sitei = subset(targetFileData, Targetsite == site)
+    pdf(paste('output/', fileName, ".",site, '.pdf', sep=''))
+	ymax = max(cyto$ChromEnd)
+	plot(0, xlim=c(0,24), ylim=c(0,ymax), type='n'
+		, axes=F, xlab='Chromosome', ylab=''
+		, main=site)
+	
+	legend('topright', col=c(3,2), legend=c('on-target','off-target'), border=NA, pch='-')	
+	xpos = 1
+
+	for (chr in chrs) {
+	    di = cyto[cyto$Chromosome == chr, ]
+	    ymax = max(di$ChromEnd)
+	    starts = ymax - di$ChromStart
+	    ends = ymax - di$ChromEnd
+	    x1 = rep(xpos-0.2, length(starts))
+	    x2 = rep(xpos+0.2, length(starts))
+	    rect(x1, starts, x2, ends, col = di$BandColor, border = NA)
+	    mtext(side=1, line=0, at=xpos, sub('chr','',chr), cex=0.9)	
+
+	    # outer
+	    rect(xpos-0.2, 0, xpos+0.2, ymax, col = NA, border = 1)
+	    cent = di[di$BandColor=='red',]
+	    cent
+	    rect(xpos-0.2, ymax - cent$ChromStart, xpos+0.2, ymax - cent$ChromEnd, col = 'white', border = 'white')
+	    lines(c(xpos-0.2, xpos+0.2), c(ymax - cent$ChromStart[1], ymax - cent$ChromEnd[2]), col =1)
+	    lines(c(xpos+0.2, xpos-0.2), c(ymax - cent$ChromStart[1], ymax - cent$ChromEnd[2]), col =1)
+
+	    ## targets
+	    siteichr = sitei[sitei$Chromosome==chr,]
+	    siteichr$off = ymax - siteichr$Start
+	    (nn = nrow(siteichr))
+	    
+	    points(rep(xpos, nn), siteichr$off, pch='-', col=ifelse(siteichr$Mismatches==0, 3, 2), cex=2.5)
+	    xpos = xpos+1
+	}
+dev.off()
+}
